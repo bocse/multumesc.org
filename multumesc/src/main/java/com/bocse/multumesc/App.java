4b738129f134901d101bb7c039a8fe4dba239481
Bogdan Bocse <bogdan.bocse@adswizz.com>
2015-11-10 18:33:44 +0200

    First working version

--- multumesc/src/main/java/com/bocse/multumesc/App.java
+++ multumesc/src/main/java/com/bocse/multumesc/App.java
@@ -3,11 +3,14 @@ package com.bocse.multumesc;
 
 
 
+import org.json.simple.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -17,14 +20,29 @@ import java.util.logging.Logger;
 public class App 
 {
     final static Logger logger = Logger.getLogger(App.class.toString());
-    public static void main( String[] args ) throws IOException {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        //System.out.println( "Hello World!" );
         //Document doc2= Jsoup.
-        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-        Elements newsHeadlines = doc.select("#mp-itn b a");
-        for (int i = 0; i < newsHeadlines.size(); i++) {
-            logger.info(newsHeadlines.get(i).text());
+
+        PresenceParser pp=new PresenceParser();
+        //Document doc2=pp.getDocument(417L, 170L);
+        //Long personId=2L;
+        Long maxPerson=418L;
+        for (Long personId=1L; personId<=maxPerson; personId++) {
+            List<Map<String, Object>> map = pp.getPerson(personId);
+            JSONObject object = new JSONObject();
+            object.put("personId", personId);
+            object.put("voteData", map);
+            JsonSerializer jser = new JsonSerializer();
+            jser.serialize("C:\\Temp\\Cdep\\", personId, object);
+            Thread.sleep((int)(5000+System.nanoTime()%2000));
         }
-        logger.info(newsHeadlines.last().text());
+//        System.exit(0);
+//        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+//        Elements newsHeadlines = doc.select("#mp-itn b a");
+//        for (int i = 0; i < newsHeadlines.size(); i++) {
+//            logger.info(newsHeadlines.get(i).text());
+//        }
+//        logger.info(newsHeadlines.last().text());
     }
 }
