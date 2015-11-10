e08a77b56275d937b7fc0ab766d8b18ef0d5cc2d
Bogdan Bocse <bogdan.bocse@adswizz.com>
2015-11-10 18:52:52 +0200

    Coarse retrial mechanism

--- multumesc/src/main/java/com/bocse/multumesc/App.java
+++ multumesc/src/main/java/com/bocse/multumesc/App.java
@@ -24,19 +24,39 @@ public class App
         //System.out.println( "Hello World!" );
         //Document doc2= Jsoup.
 
-        PresenceParser pp=new PresenceParser();
-        //Document doc2=pp.getDocument(417L, 170L);
-        //Long personId=2L;
-        Long maxPerson=418L;
-        for (Long personId=1L; personId<=maxPerson; personId++) {
-            List<Map<String, Object>> map = pp.getPerson(personId);
-            JSONObject object = new JSONObject();
-            object.put("personId", personId);
-            object.put("voteData", map);
-            JsonSerializer jser = new JsonSerializer();
-            jser.serialize("C:\\Temp\\Cdep\\", personId, object);
-            Thread.sleep((int)(5000+System.nanoTime()%2000));
-        }
+            PresenceParser pp = new PresenceParser();
+            //Document doc2=pp.getDocument(417L, 170L);
+            //Long personId=2L;
+            Long firstPerson=120L;
+            Long maxPerson = 418L;
+            int maxRetry=10;
+
+            for (Long personId = firstPerson; personId <= maxPerson; personId++) {
+                int retryIndex=0;
+                boolean success=false;
+                while (!success && retryIndex<maxRetry)
+                {
+                try{
+                List<Map<String, Object>> map = pp.getPerson(personId);
+                JSONObject object = new JSONObject();
+                object.put("personId", personId);
+                object.put("voteData", map);
+                JsonSerializer jser = new JsonSerializer();
+                jser.serialize("C:\\Temp\\Cdep\\", personId, object);
+                Thread.sleep((int) (5000 + System.nanoTime() % 2000));
+                success=true;
+                }
+                catch (Exception ex) {
+                    logger.warning("Error " + ex.getMessage());
+                    logger.info("Retrying " + retryIndex + "/" + maxRetry);
+                    retryIndex++;
+                    Thread.sleep(10000);
+
+                }
+                }
+            }
+
+
 //        System.exit(0);
 //        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
 //        Elements newsHeadlines = doc.select("#mp-itn b a");
